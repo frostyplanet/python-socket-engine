a2589589ac42ec2abec4526432aacfd25164013a
Adam Ning (Plan) <frostyplanet@gmail.com>
2013-05-28 13:34:56 +0800

    add benchmark

---
+++ test/benchmark1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from test_server import *
+
+def main ():
+    Log ("client", config=conf)
+    Log ("server", config=conf)
+    server = start_unblock_server ()
+    time.sleep (1)
+    test_client ()
+
+if __name__ == '__main__':
+    main ()
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 :
